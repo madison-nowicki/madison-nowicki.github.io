b6358c554e46e2fa07932a4ab64985616b41ec9e
madison-nowicki <mano7114@colorado.edu>
2022-04-03 20:45:39 -0600

    Assignment 11: JavaScript 1

---
+++ js/week11-assignment.js
@@ -0,0 +1,6 @@
+function changeColor(newColor) {
+  var elem = document.getElementById('titlename');
+  elem.style.color = #E05194;
+}
+
+document.querySelector('h1').addEventListener('hover', changeColor);
